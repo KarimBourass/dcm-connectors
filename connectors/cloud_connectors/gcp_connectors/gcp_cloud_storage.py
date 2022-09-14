5251301b4500b30ce197f5a99ea621e1ef12a355
karim bourass <bourasskarim9@gmail.com>
2022-09-14 16:01:09 +0100

    Update upload txt

--- connectors/cloud_connectors/gcp_connectors/gcp_cloud_storage.py
+++ connectors/cloud_connectors/gcp_connectors/gcp_cloud_storage.py
@@ -19,36 +19,37 @@ class GCPCloudStorageConnector(Connector, GCPConnector):
 
     # TODO: Probably its better to get file_Id and sheet_id then load it
     def upload_df(self, df, *args, **kwargs):
+        print("kwargs", kwargs)
+        file_name = kwargs["file_name"]
+        file_type = kwargs["file_type"]
+        path = f"{file_name}.{file_type}"
         if not df.empty:
             bucket_name = kwargs["bucket_name"]
             file_name = kwargs["file_name"]
             client = storage.Client(credentials=self.credentials)
             bucket = client.get_bucket(bucket_name)
-            if "target_fields" in args.keys():
-                self.upload_txt(df, args["args"], bucket)
+            if "target_fields" in kwargs.keys() and file_type == "txt":
+                self.upload_txt(df, kwargs["target_fields"], bucket, path)
                 return
 
-            bucket.blob(file_name).upload_from_string(df.to_csv(), 'text/csv')
+            bucket.blob(path).upload_from_string(df.to_csv(), 'text/csv')
 
+    def upload_txt(self, df, target_fields, bucket, path):
+        with open('output.txt', 'w') as f:
+            for index, row in df.iterrows():
+                for map_row in target_fields:
+                    col_name = map_row['label']
+                    if col_name in df.columns:
+                        col_value = str(row[col_name])
+                    else:
+                        col_value = ""
 
+                    new_column = self.build_column(col_value, map_row['size'])
+                    f.write(new_column)
 
+                f.write('\n')
 
-    def upload_txt(self, df, target_fields, bucket):
-            with open('output.txt', 'w') as f:
-                for index, row in df.iterrows():
-                    for map_row in target_fields:
-                        col_name = map_row['name']
-                        if col_name in df:
-                            col_value = str(row[col_name])
-                        else:
-                            col_value = ""
-
-                        new_column = self.build_column(col_value, map_row['size'])
-                        f.write(new_column)
-
-                    f.write('\n')
-
-            bucket.blob('output.txt').upload_from_file(file_obj=f, rewind=True)
+            bucket.blob(path).upload_from_filename("output.txt")
 
 
 
