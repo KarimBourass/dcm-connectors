84eec8d434018f55f0a1d8542c1f4f61cad9a34a
karim bourass <bourasskarim9@gmail.com>
2022-09-15 14:00:23 +0100

    Update gcp cloud storage

--- connectors/cloud_connectors/gcp_connectors/gcp_cloud_storage.py
+++ connectors/cloud_connectors/gcp_connectors/gcp_cloud_storage.py
@@ -1,5 +1,4 @@
-import pandas as pd
-
+import os
 from connectors.cloud_connectors.gcp_connectors.gcp_connecor import GCPConnector
 from connectors.connector import Connector
 
@@ -19,7 +18,6 @@ class GCPCloudStorageConnector(Connector, GCPConnector):
 
     # TODO: Probably its better to get file_Id and sheet_id then load it
     def upload_df(self, df, *args, **kwargs):
-        print("kwargs", kwargs)
         file_name = kwargs["file_name"]
         file_type = kwargs["file_type"]
         path = f"{file_name}.{file_type}"
@@ -35,30 +33,29 @@ class GCPCloudStorageConnector(Connector, GCPConnector):
             bucket.blob(path).upload_from_string(df.to_csv(), 'text/csv')
 
     def upload_txt(self, df, target_fields, bucket, path):
-        with open('output.txt', 'w') as f:
+        with open('output.txt', 'a+') as f:
+            f.seek(0)
             for index, row in df.iterrows():
                 for map_row in target_fields:
-                    col_name = map_row['label']
-                    if col_name in df.columns:
+                    col_name = map_row['name']
+                    if col_name in df:
                         col_value = str(row[col_name])
                     else:
                         col_value = ""
 
                     new_column = self.build_column(col_value, map_row['size'])
                     f.write(new_column)
-
                 f.write('\n')
 
-            bucket.blob(path).upload_from_filename("output.txt")
-
-
+        bucket.blob(path).upload_from_filename('output.txt')
+        os.remove("output.txt")
 
     def build_column(self, column, length):
-        if len(column) > length:
-            return column
-        elif length == len(column):
-            return column
-        else:
-            diff = length - len(column)
-            new_column = column + " "*diff
-            return new_column
+      if len(column) > length:
+        return column
+      elif length == len(column):
+        return column
+      else:
+        diff = length - len(column)
+        new_column = column + " " * diff
+        return new_column
