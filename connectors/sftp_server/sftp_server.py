1d0516dec1281e90e0f76826c6d442b22f4ff11e
Karim Bourass <bourasskarim9@gmail.com>
2023-05-25 21:47:00 +0100

    update seft server

--- connectors/sftp_server/sftp_server.py
+++ connectors/sftp_server/sftp_server.py
@@ -1,7 +1,7 @@
 from connectors.connector import Connector
 from connectors.sftp_server.sftp_connector import SFTPServerConnector
 import os
-
+from datetime import datetime
 
 class SftpServer(Connector, SFTPServerConnector):
 
@@ -23,6 +23,9 @@ class SftpServer(Connector, SFTPServerConnector):
     def upload_txt(self, df, target_fields, path):
         with open('output.txt', 'a+') as f:
             f.seek(0)
+            start_row = f"00000000NOMPARTN CREDITQUOT 8    {datetime.today().strftime('%Y%m%d')}00734161                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     "
+            f.write(start_row)
+            f.write('\r\n')
             for index, row in df.iterrows():
                 for map_row in target_fields:
                     col_name = map_row['name']
@@ -31,18 +34,41 @@ class SftpServer(Connector, SFTPServerConnector):
                     else:
                         col_value = ""
 
-                    new_column = self.build_column(col_value, map_row['size'])
-                    f.write(new_column)
-                f.write('\n')
+                    line = line + self.build_column(col_value, map_row)
+                    
+                new_line = self.build_line(line)
+                f.write(new_line)
+                f.write('\r\n')
+                
+            end_row = f"99999999NOMPARTN CREDIT     8    {datetime.today().strftime('%Y%m%d')}0073416100000014                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             "
+            f.write(end_row)
+            f.write('\r\n')
+                   
         self._connection.put(localpath='output.txt', remotepath=path, confirm=True)
         os.remove("output.txt")
 
-    def build_column(self, column, length):
-        if len(column) > length:
-            return column
-        elif length == len(column):
+    def build_column(column, map_row):
+        length = map_row['size']
+        col_length = len(column)
+        if col_length > length:
+            return column[:length]
+        elif length == col_length:
             return column
         else:
-            diff = length - len(column)
-            new_column = column + " " * diff
+            diff = length - col_length
+            if map_row['type'] == "int":
+                new_column = ("0" * diff) + column
+            else:
+                new_column = column + " " * diff
             return new_column
+
+    def build_line(line, length=870):
+        line_length = len(line)
+        if line_length > length:
+            return line[0:length]
+        elif length == line_length:
+            return line
+        else:
+            diff = length - line_length
+            new_line = line + " " * diff
+            return new_line
