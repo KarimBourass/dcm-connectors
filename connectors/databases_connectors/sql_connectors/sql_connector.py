98e4e185d33570bf0e0a3fd2f043019fd65caf02
Karim Bourass <bourasskarim9@gmail.com>
2023-05-29 13:29:12 +0100

    Update sql_connector.py - Reveret changes

--- connectors/databases_connectors/sql_connectors/sql_connector.py
+++ connectors/databases_connectors/sql_connectors/sql_connector.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from urllib.parse import quote
-import os
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -13,8 +12,8 @@ class SqlConnector(Connector):
 
     def __init__(self, host, user, password, port, database):
         self.host = host
-        self.user = os.getenv(str(user))
-        self.password = os.getenv(str(password))
+        self.user = user
+        self.password = password
         self.port = port
         self.database = database
         self.driver = None
