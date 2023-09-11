bc6be99d4e890e9d156e1fa575fd7367dad576d8
Karim Bourass <bourasskarim9@gmail.com>
2023-09-11 18:42:31 +0100

    Update company logic

--- connectors/hubspot_connectors/entities/company.py
+++ connectors/hubspot_connectors/entities/company.py
@@ -6,9 +6,9 @@ from flatten_json import flatten
 import numpy as np
 import math
 
-CHUNK_SIZE = 1
-COMPANY_BATCH_URL = "https://api.hubapi.com/companies/v1/company/batch"
-COMPANY_PROPERTIES_URL = "https://api.hubapi.com/properties/v1/company/properties"
+CHUNK_SIZE = 100
+COMPANY_BATCH_URL = "https://api.hubapi.com/crm/v3/objects/companies/batch/create"
+COMPANY_PROPERTIES_URL = "https://api.hubapi.com/properties/v1/companies/properties/"
 
 
 class Company():
@@ -16,41 +16,48 @@ class Company():
     def __init__(self, token):
         self.headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(token)}
         
-    def get_COMPANY_properties(self):
+    def get_company_properties(self):
         check_response = requests.get(COMPANY_PROPERTIES_URL, headers=self.headers)
         properties = check_response.json()
         check_fields = [a['name'] for a in properties]
         return check_fields
     
-    def format_batch_companies(self, companies, COMPANY_properties):
+    def format_batch_companies(self, companies, company_properties):
         result = []
         for c in companies:
+            props = {}
             my_dict = {}
-            if "vid" in c.keys():
-                my_dict.update({"vid": c['vid']})
-            elif "email" in c.keys():
-                my_dict.update({"email": c['email']})
 
-            properties = [{"property": k, "value": c[k]} for k in c.keys() if k in COMPANY_properties]
-            my_dict['properties'] = properties
+            for k in c.keys():
+                if k in company_properties:
+                    props[k] = c[k]
+
+            my_dict['properties'] = props
             result.append(my_dict)
 
         # Split arrays
         result = split_array(result)
         return result
-    
+
+    def build_companies_vids(self, results, df):
+        data = [{"company_vid": r['id'], "id_sf_company": r['properties']['id_sf']} for r in results]
+        df = df.append(data, ignore_index=True)
+        return df
+
     
     # Create or Update Batch Companies
     # Note: The batch size should not exceed 1000 companies per request.
     def publish_COMPANY_batch(self, companies):
-        COMPANY_properties = self.get_COMPANY_properties()
-        bodies = self.format_batch_companies(companies, COMPANY_properties)
-        print("Bodies: {}".format(bodies))
-
+        company_properties = self.get_company_properties()
+        bodies = self.format_batch_companies(companies, company_properties)
+        df = pd.DataFrame()  # Initialize an empty DataFrame
         for body in bodies:
-            response = requests.post(COMPANY_BATCH_URL, json=list(body), headers=self.headers)
-            print("Response: {}", response.text)
-    
+            response = requests.post(COMPANY_BATCH_URL, json={"inputs": list(body)}, headers=self.headers)
+            if response.status_code == 201:
+                df = self.build_companies_vids(response.json()['results'])
+
+        return df
+
 def split_array(original_array):
     numberOfSubLists = math.ceil(len(original_array) / CHUNK_SIZE)
     arrays = np.array_split(original_array, numberOfSubLists)
